06ae9b1b5c6413c1cf08606eabbbeb3d22fdc903
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-10 22:27:50 +0200

    ℹ️ Feat(JSX) Update contacts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,10 +9,10 @@ import Filter from '../Filter/Filter';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Steve Jobs', number: '459-12-56' },
+      { id: 'id-2', name: 'Bill Gates', number: '443-89-12' },
+      { id: 'id-3', name: 'Jeff Bezos', number: '645-17-79' },
+      { id: 'id-4', name: 'Elon Musk', number: '227-91-26' },
     ],
     filter: '',
   };
